44f9cbd287d84f8ea0a9d67022a85a0aac4d660a
Weberton <faria.weberton@gmail.com>
2026-06-14 10:56:43 -0400

    add def and anonymous function examples to functions topic

--- src/clojure_basics/topics/functions.clj
+++ src/clojure_basics/topics/functions.clj
@@ -8,3 +8,14 @@
   (println "I am learning clojure")                         ; Expression 2
   (+ 1 10)                                                  ; Returns 11 (result of the last expression)
   )
+
+
+; defn is the keyword that creates a named function
+; def assigns an anonymous function (fn) to a name
+; Now we can use increment by name inside other functions
+(def increment (fn [x] (+ x 1)))
+
+(defn increment-set
+  [x]                                                       ; receives a param
+  (increment x)                                             ; calls increment with x
+  )
