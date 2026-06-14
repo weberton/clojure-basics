76bf25c5ea5f38a468cab933eb9e251e1eececeb
Weberton <faria.weberton@gmail.com>
2026-06-14 10:44:32 -0400

    add basic function example with expressions and return value

--- src/clojure_basics/topics/functions.clj
+++ src/clojure_basics/topics/functions.clj
@@ -1,1 +1,10 @@
 (ns clojure-basics.topics.functions)
+
+(defn basic-function
+  "My first basic function. It creates two expressions to print text and returns the result of the last expression.
+  Functions always return something - the last expression's value, or nil."
+  []                                                        ;No params
+  (println "My name is Tom")                                ; Expression 1
+  (println "I am learning clojure")                         ; Expression 2
+  (+ 1 10)                                                  ; Returns 11 (result of the last expression)
+  )
